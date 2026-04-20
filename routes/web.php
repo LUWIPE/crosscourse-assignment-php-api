26daeba0ae9a361ea47415a123d9597c86c5b9dd
LUWIPE <145003645+LUWIPE@users.noreply.github.com>
2026-04-20 14:43:34 +0200

    fix: add schema route to return database schema overview in JSON format

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,43 @@ Route::get('/', function () {
         'message' => 'API is running',
     ]);
 });
+
+Route::get('/schema', function () {
+    return response()->json([
+        'name' => config('app.name'),
+        'status' => 'ok',
+        'message' => 'Schema overview',
+        'schema' => [
+            'tables' => [
+                'types' => ['id', 'name', 'description', 'created_at', 'updated_at'],
+                'grades' => ['id', 'name', 'description', 'created_at', 'updated_at'],
+                'digitals' => ['id', 'name', 'created_at', 'updated_at'],
+                'products' => [
+                    'id',
+                    'name',
+                    'price',
+                    'release',
+                    'description',
+                    'stock',
+                    'img_url',
+                    'type_id',
+                    'grade_id',
+                    'digital_id',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+            'relations' => [
+                'products.type_id -> types.id',
+                'products.grade_id -> grades.id',
+                'products.digital_id -> digitals.id',
+                'Product belongsTo Type',
+                'Product belongsTo Grade',
+                'Product belongsTo Digital',
+                'Type hasMany Product',
+                'Grade hasMany Product',
+                'Digital hasMany Product',
+            ],
+        ],
+    ]);
+});
